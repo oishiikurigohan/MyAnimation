c6bd31e27f80ccf925e58fd378ac5a961f4141d1
kurigohan <kurigohan_blog-contact@yahoo.co.jp>
2017-06-06 11:22:31 +0900

    completed

--- MyAnimation/MyAnimation/GameScene.cs
+++ MyAnimation/MyAnimation/GameScene.cs
@@ -7,13 +7,12 @@ namespace MyAnimation
 {
     public class GameScene : CCScene
     {
-        private CCAnimate animate;
         private CCSprite sprite;
-        private CCRepeatForever walkRepeat;
+        private CCActionState state;
+        private SortSpriteFrames sortspriteframes;
 
         public GameScene(CCGameView gameView) : base(gameView)
         {
-            // CCSceneにCCLayerColorをセット( CCLayerColor = CCLayer + 色情報 )
             var layer = new CCLayerColor(CCColor4B.White);
             AddLayer(layer);
 
@@ -22,76 +21,69 @@ namespace MyAnimation
             string[] FileNames = assembly.GetManifestResourceNames();
 
             // 3桁以上の連番が入っているPNGをアニメーション用のファイルとする。
-            // アニメーションで使う画像ファイルは、表示する順番に3桁の連番.pngで作成し
-            // MyAnimation(PCL)/Resources/Images/Framesに入れてあります。
-            var spriteframes = new List<CCSpriteFrame>();
+            // 画像とIDを紐づけるためのリスト(spriteframesInfo)を作成する。
+            var spriteframesInfo = new List<SpriteFrameInfo>();
             var rect = new CCRect(0, 0, 300, 100);
             foreach (String FileName in FileNames)
             {
                 if (System.Text.RegularExpressions.Regex.IsMatch(FileName, @"\w*\d{3,}\w*\.png$"))
                 {
-                    // 後の処理で、レイヤーにセットするCCSpriteを生成する際に
-                    // アニメーションが構築されたCCSpriteFrameリストの1フレームを渡すため
-                    // 埋め込みリソースファイルをStreamで取得し、CCTexture2Dを生成しCCSpriteFrameを作っている。
-                    // 
-                    // ちなみにCCRectは画像ファイルのサイズ情報( 横300px、縦100p× )。
-                    // streamから画像ファイルのサイズ情報をGetしようと思ったけど
-                    // 簡単ではなさそう＆そこまで頑張る必要がないと判断しとりあえず固定。
-                    // 
-                    // やってることがおかしかったら教えてください。。。
                     var stream = assembly.GetManifestResourceStream(FileName);
                     var texture2d = new CCTexture2D(stream, CCSurfaceFormat.Color);
-                    spriteframes.Add(new CCSpriteFrame(texture2d, rect));
+                    var spriteframe = new CCSpriteFrame(texture2d, rect);
+                    spriteframesInfo.Add(new SpriteFrameInfo(spriteframe));
                 }
             }
+            sortspriteframes = new SortSpriteFrames(spriteframesInfo);
 
-            // CCRepeatForever : 親 CCAction : CCNodeにセットされると指定されたCCActionを永遠に繰り返す。
-            // CCAnimate       : 親 CCAction : セットされたCCAnimationに入っている画像を1つ1つ表示することができる。
-            // CCAnimation     : 親 Object   : CCSpriteでCCAnimationをアニメ化するにはCCAnimateを使用する。
-            // CCSprite        : 親 CCNode   : 2D画像をレイヤーに貼り付けるためのもの。
-            var animation = new CCAnimation(spriteframes, 0.1f);
-            animate = new CCAnimate(animation);
-            walkRepeat = new CCRepeatForever(animate);
-
-            // アニメーションの1フレームをセットしてSpriteを生成し、レイヤの真ん中に設置する。
-            // 何フレーム目をセットしても、配列の先頭から再生されます。そのへんの制御はCCAnimateなのかしら
-            sprite = new CCSprite(spriteframes[20]);
+            // spriteをnewするときに渡したフレームが初期画像となります。
+            var sorted_spriteframes = sortspriteframes.GetSortedSpriteFrames();
+            sprite = new CCSprite(sorted_spriteframes[0]);
             sprite.Position = new CCPoint(layer.ContentSize.Center.X, layer.ContentSize.Center.Y);
             layer.AddChild(sprite);
-
-            // Spriteアニメーションをフォーエヴァーリピートしてみる
-            var state = sprite.RunAction(walkRepeat);
-
-            // フォーエヴァーでなく回数指定の場合はこれ
-            // animation.Loops = 5;
-            // var state = sprite.RunAction(animate);
         }
 
+        // 倍速逆再生イメージをタップしたときの処理 
         public void FastRewindAnimation()
         {
-            System.Diagnostics.Debug.WriteLine("倍速逆再生をタップしました");
+            sprite.StopAction(state);
+            var reversesorted_spriteframes = sortspriteframes.GetReverseSortedSpriteFrames(sprite);
+            var animate = new CCAnimate(new CCAnimation(reversesorted_spriteframes, 0.1f));
+            animate.Duration = 1;
+            state = sprite.RepeatForever(animate);
         }
 
+        // 逆再生イメージをタップしたときの処理
         public void RewindAnimation()
         {
-            System.Diagnostics.Debug.WriteLine("逆再生をタップしました");
+            sprite.StopAction(state);
+            var reversesorted_spriteframes = sortspriteframes.GetReverseSortedSpriteFrames(sprite);
+            var animate = new CCAnimate(new CCAnimation(reversesorted_spriteframes, 0.1f));
+            animate.Duration = 2;
+            state = sprite.RepeatForever(animate);
         }
 
-        public void StopAnimation()
-        {
-            System.Diagnostics.Debug.WriteLine("停止をタップしました");
-            // ブログ用のスクリーンショットを撮るために一時的にこうしてるけどあとで直すと思う
-            sprite.Pause();
-        }
+        // 停止イメージをタップしたときの処理
+        public void StopAnimation() { sprite.StopAction(state); }
 
+        // 再生イメージをタップしたときの処理
         public void PlayAnimation()
         {
-            System.Diagnostics.Debug.WriteLine("再生をタップしました");
+            sprite.StopAction(state);
+            var sorted_spriteframes = sortspriteframes.GetSortedSpriteFrames(sprite);
+            var animate = new CCAnimate(new CCAnimation(sorted_spriteframes, 0.1f));
+            animate.Duration = 2;
+            state = sprite.RepeatForever(animate);
         }
 
+        // 倍速再生イメージをタップしたときの処理
         public void FastFowardAnimation()
         {
-            System.Diagnostics.Debug.WriteLine("倍速再生をタップしました");
+            sprite.StopAction(state);
+            var sorted_spriteframes = sortspriteframes.GetSortedSpriteFrames(sprite);
+            var animate = new CCAnimate(new CCAnimation(sorted_spriteframes, 0.1f));
+            animate.Duration = 1;
+            state = sprite.RepeatForever(animate);
         }
     }
 }
---
+++ MyAnimation/MyAnimation/SortSpriteFrames.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using CocosSharp;
+
+namespace MyAnimation
+{
+    // 画像とIDを紐づけるためのクラス
+    public class SpriteFrameInfo
+    {
+        public CCSpriteFrame SpriteFrame { get; }
+        public int TextureId { get; }
+
+        public SpriteFrameInfo(CCSpriteFrame spriteframe)
+        {
+            // privateメンバ spriteFrame.Texture.textureId の値をGetする
+            this.SpriteFrame = spriteframe;
+            var fieldinfo = spriteframe.Texture.GetType().GetTypeInfo().GetDeclaredField("textureId");
+            this.TextureId = (int)fieldinfo.GetValue(spriteframe.Texture);
+        }
+    }
+
+    public class SortSpriteFrames
+    {
+        static private List<SpriteFrameInfo> SpriteFrameInfo;
+
+        public SortSpriteFrames(List<SpriteFrameInfo> spriteframeinfo)
+        {
+            SortSpriteFrames.SpriteFrameInfo = spriteframeinfo;
+        }
+
+        // 引数で指定されたフレーム画像の次を先頭とするフレームリストを返却する
+        public List<CCSpriteFrame> GetSortedSpriteFrames(CCSprite sprite = null)
+        {
+            int IndexNo = 0;
+            var sorted_spriteframe = new List<CCSpriteFrame>();
+            if (sprite != null)
+            {
+                // 現在spriteに表示されているTextureのtextureIdを利用し、次に表示する画像の要素番号(IndexNo)を取得する
+                var fieldinfo = sprite.Texture.GetType().GetTypeInfo().GetDeclaredField("textureId");
+                int CurrentTextureId = (int)fieldinfo.GetValue(sprite.Texture);
+                IndexNo = SpriteFrameInfo.FindIndex(item => item.TextureId == CurrentTextureId) + 1;
+            }
+
+            // System.Diagnostics.Debug.WriteLine("まず[" + IndexNo + "] から [" + (SpriteFrameInfo.Count - 1) + "] までAddする。");
+            for (int i = IndexNo; i < SpriteFrameInfo.Count; i++) sorted_spriteframe.Add(SpriteFrameInfo[i].SpriteFrame);
+            int SubtractCount = SpriteFrameInfo.Count - sorted_spriteframe.Count;
+            // System.Diagnostics.Debug.WriteLine("次に[0]から " + SubtractCount + "個Addする。");
+            for (int i = 0; i < SubtractCount; i++) sorted_spriteframe.Add(SpriteFrameInfo[i].SpriteFrame);
+
+            return sorted_spriteframe;
+        }
+
+        // 引数で指定されたフレーム画像の前を先頭とする逆順のフレームリストを返却する
+        public List<CCSpriteFrame> GetReverseSortedSpriteFrames(CCSprite sprite = null)
+        {
+            int IndexNo = SpriteFrameInfo.Count - 1;
+            var sorted_spriteframe = new List<CCSpriteFrame>();
+            if (sprite != null)
+            {
+                // 現在spriteに表示されているTextureのtextureIdを利用し、次に表示する画像の要素番号(IndexNo)を取得する
+                var fieldinfo = sprite.Texture.GetType().GetTypeInfo().GetDeclaredField("textureId");
+                int CurrentTextureId = (int)fieldinfo.GetValue(sprite.Texture);
+                IndexNo = SpriteFrameInfo.FindIndex(item => item.TextureId == CurrentTextureId) - 1;
+            }
+
+            // System.Diagnostics.Debug.WriteLine("まず[" + IndexNo + "] から [0] まで逆順でAddする。");
+            for (int i = IndexNo; i >= 0;  i--) { sorted_spriteframe.Add(SpriteFrameInfo[i].SpriteFrame); }
+            int SubtractCount = sorted_spriteframe.Count;
+            // System.Diagnostics.Debug.WriteLine("次に[" + (SpriteFrameInfo.Count - 1) + "]から[" + SubtractCount + "]まで逆順にAddする。");
+            for (int i = SpriteFrameInfo.Count - 1; i >= SubtractCount; i--) sorted_spriteframe.Add(SpriteFrameInfo[i].SpriteFrame);
+
+            return sorted_spriteframe;
+        }
+    }
+}
